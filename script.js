ebd8c286944dee7a6da5b3f96c3956f116576b76
Ashyraffa3527 <raffadandilla@gmail.com>
2025-07-28 06:21:19 +0700

    added dark mode

--- script.js
+++ script.js
@@ -140,3 +140,12 @@ function closeAboutModal() {
   document.getElementById('about-modal').style.display = 'none';
 }
 
+const btn = document.getElementById('dark-mode');
+const container = document.querySelector('.container'); 
+
+btn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  container.classList.toggle('dark-mode'); 
+});
+
+
